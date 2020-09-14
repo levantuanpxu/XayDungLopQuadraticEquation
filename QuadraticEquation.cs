4181dbd318e601f7b6b313d1f02d4d6b169245fc
levantuanpxu <70925631+levantuanpxu@users.noreply.github.com>
2020-09-14 22:05:41 +0700

    XayDungLopQuadraticEquation
    XayDungLopQuadraticEquation

---
+++ QuadraticEquation.cs
@@ -0,0 +1,35 @@
+namespace XayDungLopQuadraticEquation
+{
+    internal class QuadraticEquation
+    {
+        private double a { set; get; }
+        private double b { set; get; }
+        private double c { set; get; }
+        
+        public QuadraticEquation()
+        {
+
+        }
+        public QuadraticEquation(double a,double b,double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public double GetDiscriminant()
+        {
+            double delta;
+            return delta = b * b - (4 * a * c);
+        }
+        public double GetRoot1()
+        {
+            return -b / (2.0 * a);
+        }
+        public double GetRoot2x1()
+        {
+            return (-b + System.Math.Sqrt(GetDiscriminant())) / (2 * a);
+        }
+
+        public double GetRoot2x2() => (-b - System.Math.Sqrt(GetDiscriminant())) / (2 * a);
+    }
+}
